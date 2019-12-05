236b676366116c4f9aedf6bf9ca7ed6d419fb7dd
Vlad <srvladyk555@gmail.com>
2019-12-05 16:52:52 +0200

    added immutability-helper

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import './Form.css';
 import Input from "./part/Input/Input";
+import update from 'immutability-helper';
+
+import './Form.css';
+
 
 class Form extends Component {
     constructor(props) {
@@ -27,14 +30,13 @@ class Form extends Component {
 
     handler = (e) => {
         const {name, value} = e.target;
-
-        this.setState({
-            newUser: {
-                [name]: value,
+        const newState = update(this.state, {
+                newUser: {
+                    [name] : {$set :value}
+                }
             }
-        });
-
-        console.log(this.state.newUser)
+        )
+        this.setState(newState);
     };
 
     render() {
@@ -46,6 +48,41 @@ class Form extends Component {
                     handler={this.handler}
                     placeholder='Enter your first name'
                     value={this.state.newUser.firstName}
+                    type='text'/>
+                <Input
+                    name='secondName'
+                    title='Second Name'
+                    handler={this.handler}
+                    placeholder='Enter your second name'
+                    value={this.state.newUser.secondName}
+                    type='text'/>
+                <Input
+                    name='userName'
+                    title='User Name'
+                    handler={this.handler}
+                    placeholder='Enter your user name'
+                    value={this.state.newUser.userName}
+                    type='text'/>
+                <Input
+                    name='email'
+                    title='Email'
+                    handler={this.handler}
+                    placeholder='Enter your email'
+                    value={this.state.newUser.email}
+                    type='text'/>
+                <Input
+                    name='password'
+                    title='Password'
+                    handler={this.handler}
+                    placeholder='Enter your password'
+                    value={this.state.newUser.password}
+                    type='text'/>
+                <Input
+                    name='repeatPassword'
+                    title='Repeat your Password'
+                    handler={this.handler}
+                    placeholder='Enter your password again'
+                    value={this.state.newUser.repeatPassword}
                     type='text'/>
             </form>
         );
